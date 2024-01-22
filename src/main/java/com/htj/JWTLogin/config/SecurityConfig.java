a917a37f4e375d4a65e7918a4aa55fddd9587d2e
hantaejun <plushan98@naver.com>
2024-01-22 16:36:20 +0900

    JWT 로그인 API 구현 (완)

--- src/main/java/com/htj/JWTLogin/config/SecurityConfig.java
+++ src/main/java/com/htj/JWTLogin/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.htj.JWTLogin.config;
 
+import com.htj.JWTLogin.jwt.JWTFilter;
+import com.htj.JWTLogin.jwt.JWTUtil;
 import com.htj.JWTLogin.jwt.LoginFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,10 +19,13 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
     //AuthenticationManager가 인자로 받을 AuthenticationConfiguraion 객체 생성자 주입
     private final AuthenticationConfiguration authenticationConfiguration;
+    //JWTUtil 주입
+    private final JWTUtil jwtUtil;
 
-    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration) {
+    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration, JWTUtil jwtUtil) {
 
         this.authenticationConfiguration = authenticationConfiguration;
+        this.jwtUtil = jwtUtil;
     }
 
     //AuthenticationManager Bean 등록
@@ -41,11 +46,11 @@ public class SecurityConfig {
         http
                 .csrf((auth) -> auth.disable());
 
-        //From 로그인 방식 disable
+        //Form 로그인 방식 disable => form 로그인 방식은 사용하지 않겠다.
         http
                 .formLogin((auth) -> auth.disable());
 
-        //http basic 인증 방식 disable
+        //http basic 인증 방식 disable => http basic 인증 방식은 사용하지 않겠다.
         http
                 .httpBasic((auth) -> auth.disable());
 
@@ -55,16 +60,33 @@ public class SecurityConfig {
                         .requestMatchers("/login", "/", "/join").permitAll()
                         .requestMatchers("/admin").hasRole("ADMIN")
                         .anyRequest().authenticated());
+        /*
+            로그인,main,회원가입에서는 모두 허용
+            admin페이지는 "ADMIN"이란 권한을 가진 사용자만 접근 가능
+            그 외 나머지는 authenticated(인증된) 사용자 접근 가능
+         */
+
+
+        //JWTFilter 등록
+        http
+                .addFilterBefore(new JWTFilter(jwtUtil), LoginFilter.class);
 
         //필터 추가 LoginFilter()는 인자를 받음 (AuthenticationManager() 메소드에 authenticationConfiguration 객체를 넣어야 함) 따라서 등록 필요
+        //AuthenticationManager()와 JWTUtil 인수 전달
         http
-                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class);
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil), UsernamePasswordAuthenticationFilter.class);
 
-
+        /*
+        .addFilterAt(Filter filter, Class<? extends Filter> atFilter) => 원하는 자리에
+        .addFilterBefore(Filter filter, Class<? extends Filter> beforeFilter) => 해당하는 필터 전에
+        .addFilterAfter(Filter filter, Class<? extends Filter> afterFilter) => 해당하는 필터 이후에
+              등록할 필터 ------------   ----------------------------------- before/at/after 위치
+         */
         //세션 설정
         http
                 .sessionManagement((session) -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        // JWT 방식에서는 session을 항상 STATELESS항 상태로 관리한다.
 
         return http.build();
     }
---
+++ src/main/java/com/htj/JWTLogin/service/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.htj.JWTLogin.service;
+
+import com.htj.JWTLogin.dto.CustomUserDetails;
+import com.htj.JWTLogin.entity.UserEntity;
+import com.htj.JWTLogin.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+
+        this.userRepository = userRepository;
+    }
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        //DB에서 조회
+        UserEntity userData = userRepository.findByUsername(username);
+
+        if (userData != null) {
+
+            //UserDetails에 담아서 return하면 AutneticationManager가 검증 함
+            return new CustomUserDetails(userData);
+        }
+
+        return null;
+    }
+}
